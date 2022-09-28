cf65cab4a00cdd95215bde84f5ac4a32c36684e3
YuryShageev <schageev.yur@yandex.ru>
2022-09-28 16:46:29 +0300

    Третий блок разбора 1 части условных операторов с Бизиным

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1. Блок 2
+        System.out.println("Task 1. Block 3");
+        int yearsOld = 16;
+        if (yearsOld >= 2 && yearsOld <= 6) {
+            System.out.println("Если возраст человека " + yearsOld + " ему нужно ходить в садик");
+        } else if (yearsOld>=7 && yearsOld<=18) {
+            System.out.println("Если возраст человека " + yearsOld + " ему нужно ходить в школу");
+        } else if (yearsOld>18 && yearsOld<24) {
+            System.out.println("Если возраст человека " + yearsOld + " ему нужно ходить в университет");
+        } else if (yearsOld>=24) {
+            System.out.println("Если возраст человека " + yearsOld + " ему нужно ходить на работу");
+        }
     }
 }
