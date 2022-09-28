1a4f6adbafe977364a6fc35170e4bb00eadfddf4
YuryShageev <schageev.yur@yandex.ru>
2022-09-28 16:47:32 +0300

    Третий блок разбора 1 части условных операторов с Бизиным

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
         // Задание 1. Блок 2
         System.out.println("Task 1. Block 3");
-        int yearsOld = 16;
+        int yearsOld = 4;
         if (yearsOld >= 2 && yearsOld <= 6) {
-            System.out.println("Если возраст человека " + yearsOld + " ему нужно ходить в садик");
+            System.out.println("Если возраст человека " + yearsOld + ", ему нужно ходить в садик");
         } else if (yearsOld>=7 && yearsOld<=18) {
-            System.out.println("Если возраст человека " + yearsOld + " ему нужно ходить в школу");
+            System.out.println("Если возраст человека " + yearsOld + ", ему нужно ходить в школу");
         } else if (yearsOld>18 && yearsOld<24) {
-            System.out.println("Если возраст человека " + yearsOld + " ему нужно ходить в университет");
+            System.out.println("Если возраст человека " + yearsOld + ", ему нужно ходить в университет");
         } else if (yearsOld>=24) {
-            System.out.println("Если возраст человека " + yearsOld + " ему нужно ходить на работу");
+            System.out.println("Если возраст человека " + yearsOld + ", ему нужно ходить на работу");
         }
     }
 }
